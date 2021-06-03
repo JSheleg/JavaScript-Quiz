5387f75d1cd9115f60baf461c6cddc957e6b1012
JSheleg <jessica.sheleg@gmail.com>
2021-06-02 22:54:15 -0500

    add elements of first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,23 +25,27 @@ var questions = [
     }    
 ];
 
+//Initialized variables
 
 var timer = document.getElementById('countdown');
 var start = document.querySelector("#startGame");
-var choice =document.getElementById("id");
-
-
+var buttonChoices =document.querySelectorAll("#id");
+var scoreEl = document.getElementById('count')
 var qCounter = -1;
 var finalAnswer;
 var selectedChoice;
 var score = 0;
 var chosenbtn;
+var selectedChoice;
 var m;
+var timeLeft = 100;
 
 
+
+// Function to start counter with 100s on the clock to beat the test
 
 var countdown = function(){
-    var timeLeft = 100;
+    //var timeLeft = 100;
 
     var timeInterval = setInterval(function(){
         if(timeLeft > 1){
@@ -59,40 +63,90 @@ var countdown = function(){
     }, 1000);
 }
 
+// function to establish buttons and add event-listeners to listen for the selectedChoice
 var establishButtons = function(){
+
+    //create div with class button-wrapper + qcounter and question div and header
+    var questionPrompt = document.createElement('h2');
+    questionPrompt.setAttribute("style", 'text-align:center;')
+    
+    
+    document.body.appendChild(questionPrompt);
+
     var quizContainer = document.createElement('div');
-    quizContainer.setAttribute("class", "button-wrapper");
+    quizContainer.setAttribute("class", "button-wrapper "+ qCounter);
+    quizContainer.setAttribute("style", 'text-align:center;')
+    console.log("qCounter: " + qCounter); 
 
     for(var i = 0; i< questions[qCounter].multipleChoice.length; i++){
-        console.log(qCounter); 
+        //create button and set id to i counter
         var btnChoices = document.createElement("BUTTON");
         btnChoices.setAttribute("id", i);
-        quizContainer.appendChild(btnChoices);
-        document.body.append(quizContainer);
-        document.getElementById(i).textContent = questions[qCounter].multipleChoice[i];
+
+        //append btnChoices to quiz container and quizContainer to document
         
+        document.body.appendChild(quizContainer)
+        quizContainer.appendChild(btnChoices);
+        
+
+
+        //target each of the btnChoices inner text and input questions[qcounter].multipleChoices[i}]
+        questionPrompt.innerText =questions[qCounter].prompt
+
+        btnChoices.innerText = questions[qCounter].multipleChoice[i];
+        finalAnswer= questions[qCounter].answer
+        console.log("final answer = " + finalAnswer);
+        selectedChoice = '';
+        console.log(selectedChoice + "loop selected choice")
+        btnChoices.addEventListener('click',function(){
+            selectedChoice = document.getElementById(this.id).innerText;
+            console.log(selectedChoice);
+            alert("button clicked " + this.id)
+            console.log("selected choice" + selectedChoice);
+            solutions();
+        });  
     }
-    //debugger;
+    
     finalAnswer= questions[qCounter].answer
-    console.log("final answer = " + finalAnswer);
-     
+    console.log("final answer = " + finalAnswer); 
 }
 
 var solutions = function(){
-    // selectedChoice = document.getElementById(this.id).textContent;
-    //     alert("button clicked " + this.id)
-    //     alert("button" + i + "clicked");
+ 
     if(finalAnswer === selectedChoice){
+        console.log(finalAnswer + "final answer")
+        console.log(selectedChoice)
         alert("correct")
-        //qcounter ++;
-        //score ++;
-
+        console.log(buttonChoices[qCounter]);
+        document.getElementsByClassName('.button-wrapper qCounter' ).style.display ="none";
+        //buttonChoices.setAttribute("style", 'display:none');
+        //buttonChoices = document.getElementsByTagName("div").style.visibility = "hidden";
+        score ++;
+        scoreEl.textContent = score;
+        qCounter ++
+        console.log(qCounter + " Question");
+        
+        establishButtons();
     }
     else{
         alert("incorrect answer")
+        console.log(finalAnswer + "final answer")
+        console.log(selectedChoice)
+        if(score == 0){
+            alert("You lost the game")
+            clearInterval(timeInterval);
+        }
+        else{
+            score --;
+            scoreEl.textContent = score;
+            timeLeft = timeLeft - 10;
+            qCounter ++;
+            console.log(qCounter + " Question");
+            establishButtons();
+        }
+        
         //qcounter ++;
-        //score --;
-        //timeLeft = timeLeft - 10
+        
     }
 
 }
@@ -105,12 +159,6 @@ start.addEventListener("click",function(){
     countdown();
     qCounter ++;
     establishButtons();
-   
 });
 
 
-choice.addEventListener("click",function(){
-    choice = document.getElementById("#id");
-    console.log(choice);
-    alert("button clicked")
-})
